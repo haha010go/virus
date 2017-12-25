d0af8334c6a405fbef7fd60fe6ebb08f53adb048
haha010go <1005276278@qq.com>
2017-12-25 14:59:39 +0800

    change some small parts

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,10 +4,11 @@ from .models import Question
 
 # Create your views here.
 def index(request):
-    print(request)
-    latest_question_list= Question.objects.order_by('-pub_date')[:5]
-    output=','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    # print(request)
+    # latest_question_list= Question.objects.order_by('-pub_date')[:5]
+    # output=','.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    return HttpResponse("helloworld")
 
 def detail(request,question_id):
     return HttpResponse("You are looking at question %s." % question_id)
